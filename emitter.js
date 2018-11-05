4b758eaaeaff0638cbb102dabcd5e2c30676cca1
ZaoAsya <zaostrovskaya.asya@yandex.ru>
2018-11-05 15:19:07 +0500

    without star

--- emitter.js
+++ emitter.js
@@ -4,13 +4,25 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let subscriptions = [];
+    const parseEvent = event => {
+        const events = [];
+        let sumEvent = '';
+        event.split('.').forEach(item => {
+            sumEvent = sumEvent ? `${sumEvent}.${item}` : item;
+            events.push(sumEvent);
+        });
+
+        return events.reverse();
+    };
+
     return {
 
         /**
@@ -18,26 +30,47 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on(event, context, handler) {
+            if (!event) {
+                return this;
+            }
+            handler = handler.bind(context);
+            const newSubscribe = { context, handler };
+            subscriptions.push({ event, ...newSubscribe });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
-        off: function (event, context) {
-            console.info(event, context);
+        off(event, context) {
+            subscriptions = subscriptions.filter(subscription => {
+                return !((event === subscription.event ||
+                    subscription.event.startsWith(`${event}.`)) &&
+                    subscription.context === context);
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
-        emit: function (event) {
-            console.info(event);
+        emit(event) {
+            const events = parseEvent(event);
+            events.forEach(ev => {
+                subscriptions.forEach(sub => sub.event === ev && sub.handler());
+            });
+
+            return this;
         },
 
         /**
